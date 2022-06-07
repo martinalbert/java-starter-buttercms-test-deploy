5f9bdc9fc9a0f5c0917247992cca60a4f40d9c27
Jan Štefánik <jan.stefanik@applifting.cz>
2022-06-07 09:36:13 +0200

    Change category SEO description to match the expected description

--- src/main/java/com/buttercms/springstarterbuttercms/controller/dto/ConstDtoValues.java
+++ src/main/java/com/buttercms/springstarterbuttercms/controller/dto/ConstDtoValues.java
@@ -4,7 +4,7 @@ public class ConstDtoValues {
     public static String BLOG_SEO_TITLE = "Sample Blog - All Posts";
     public static String BLOG_SEO_DESCRIPTION = "Sample blog powered by ButterCMS, showing all posts.";
     public static String BLOG_CATEGORY_SEO_TITLE = "Sample Blog - category: ";
-    public static String BLOG_CATEGORY_SEO_DESCRIPTION = "Sample Blog - category: ";
+    public static String BLOG_CATEGORY_SEO_DESCRIPTION = "Sample blog powered by ButterCMS, showing category: ";
     public static String BLOG_TAG_SEO_TITLE = "Sample Blog - tag: ";
     public static String BLOG_TAG_SEO_DESCRIPTION = "Sample blog powered by ButterCMS, showing tag: ";
     public static String BLOG_SEARCH_SEO_TITLE = "Sample Blog - search results for ";
