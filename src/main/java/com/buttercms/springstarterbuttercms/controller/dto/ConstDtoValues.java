e71ef6704c6e14d4f4eb06c308718ac178fa614c
ViolanteCodes <maria@buttercms.com>
2022-11-21 10:29:05 -0500

    fix `by tag` text for rainforest

--- src/main/java/com/buttercms/springstarterbuttercms/controller/dto/ConstDtoValues.java
+++ src/main/java/com/buttercms/springstarterbuttercms/controller/dto/ConstDtoValues.java
@@ -10,7 +10,7 @@ public class ConstDtoValues {
     public static String BLOG_SEARCH_SEO_TITLE = "Sample Blog - search results for ";
     public static String BLOG_SEARCH_SEO_DESCRIPTION = "Sample blog powered by ButterCMS, showing search results for ";
     public static String ALL_BLOGS = "All Blog Posts";
-    public static String BLOGS_BY_CATEGORY = "Blog Posts By Category";
-    public static String BLOGS_BY_TAG = "Blog Posts By Tag";
+    public static String BLOGS_BY_CATEGORY = "Blog Posts by Category";
+    public static String BLOGS_BY_TAG = "Blog Posts by Tag";
     public static String SEARCH_RESULTS = "Search Results";
 }
